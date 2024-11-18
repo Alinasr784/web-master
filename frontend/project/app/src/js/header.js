d227842cdb31c11dd2980e4112407f3128ab716d
Ali Nasreldin Ali <157469044+Alinasr784@users.noreply.github.com>
2024-11-18 21:44:53 +0000

    Header v1.1

--- frontend/project/app/src/js/header.js
+++ frontend/project/app/src/js/header.js
@@ -1,38 +1,88 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { initializeApp } from "firebase/app";
 import { firebaseConfig } from "./firebase";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars, faUser , faTags , faFire , faCoins , faChild , faHeartPulse , faWheatAwnCircleExclamation , faHamburger,faBowlFood,faDrumSteelpan,faArrowRightFromBracket , faListDots, faHeart} from "@fortawesome/free-solid-svg-icons";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 import "../css/header.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
+
 const app = initializeApp(firebaseConfig);
 
-function Normal(){
-  return(
+function Normal() {
+  const [logoVisible, setLogoVisible] = useState(true); // للتحكم في عرض الشعار
+  const [message, setMessage] = useState(null); // نص الرسالة
+  const [messageAction, setMessageAction] = useState(null); // دالة الرسالة
+  const [messageClass, setMessageClass] = useState(""); // كلاس الرسالة للأنيميشن
+  const showMessage = (text, duration, action) => {
+    setMessage(text); // تعيين نص الرسالة
+    setMessageAction(() => action); // تعيين الدالة
+    setMessageClass("show"); // تطبيق كلاس الظهور
+    setLogoVisible(false); // إخفاء الشعار
+
+    setTimeout(() => {
+      setMessageClass("hide"); // تطبيق كلاس الاختفاء
+      setTimeout(() => {
+        setMessage(null); // إزالة الرسالة
+        setLogoVisible(true); // إعادة عرض الشعار
+      }, 500); // وقت الأنيميشن
+    }, duration);
+  };
+
+  useEffect(() => {
+    showMessage("Added To Card", 3000, () => {
+      console.log("Message clicked!");
+    });
+
+    const tes = setTimeout(() => {
+      showMessage("Done Ali", 3000, () => {
+        console.log("Message clicked!");
+      });
+    }, 8000);
+
+    // تنظيف التايمر عند إعادة التقديم أو عند إلغاء مكون
+    return () => clearTimeout(tes);
+  }, []);
+
+    
+  return (
     <div className="contain">
       <div className="contain-left">
         <div className="menu">
           <FontAwesomeIcon icon={faBars} />
         </div>
       </div>
+
       <div className="contain-center">
-        <div className="logo">PIZZA Time</div>
+        {message ? (
+          <div
+            className={`message ${messageClass}`}
+            onClick={messageAction}
+          >
+            {message}
+          </div>
+        ) : (
+          <div className={`logo ${logoVisible ? "show" : "hide"}`}>
+            PIZZA Time
+          </div>
+        )}
       </div>
+
       <div className="contain-right">
-        <div className="meAvatae">
-          <img src="/assets/images/Profile.png"/>
+        <div className="meAvatar">
+          <img src="/assets/images/Profile.png" alt="Profile" />
         </div>
       </div>
-    </div>
-  )
-}
-function Header() {
-  return (
-    <div className="header">
-      <Normal/>
     </div>
   );
 }
 
-export default Header;
+function Header() {
+  return (
+    <div className="header">
+      <Normal />
+    </div>
+  );
+}
+
+export default Header; 
