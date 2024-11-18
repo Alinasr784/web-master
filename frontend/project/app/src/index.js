ed72bcff38a33d60198f1bb099322c5d5e4cf114
Ali Nasreldin Ali <157469044+Alinasr784@users.noreply.github.com>
2024-11-18 17:43:50 +0000

    Header v1

--- frontend/project/app/src/index.js
+++ frontend/project/app/src/index.js
@@ -1,18 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Header from './js/header'
-import Carousel from './js/carousel'
-import AllMenu from './js/allMenu'
-import Sections from './js/section'
 import "./css/home.css"
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <div className='home'>
       <Header/>
-      <Carousel/>
-      <Sections/>
-      <AllMenu/>
     </div>
   </React.StrictMode>
 );
